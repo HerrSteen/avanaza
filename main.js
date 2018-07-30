6ee66b05bb04c4872c3a95f1713b5650ee84bccd
Johan Steen <pastor.steen@gmail.com.com>
2018-07-30 22:39:08 +0200

    cleanup ui

--- main.js
+++ main.js
@@ -8,19 +8,29 @@ const orderForm = document.getElementsByClassName("orderForm")[0];
 const isBuySite = window.location.href.includes("kop");
 const isSellSite = window.location.href.includes("salj");
 
+const removeElement = (className) => {
+  const el = document.getElementsByClassName(className)[0];
+  if (!el) return null;
+  el.parentNode.removeChild(el);
+};
+
+const el = document.getElementsByClassName("grid_7")[0];
+el.classList.add("grid_12");
+
 addSpread(spreadContainer);
 
 if (isBuySite) {
   const button = createButton(buttonContainer, "Säljarens pris", "buy", buttonClick);
   addSumInput(inputContainer);
   createLink(orderForm, "sälj", "salj");
-  const sellButton = document.getElementsByClassName("sellBtn")[0];
-  sellButton.parentNode.removeChild(sellButton);
+  removeElement("sellBtn");
+  removeElement("transferLinks");
+  removeElement("grid_5");
 
 } else if (isSellSite) {
   createButton(buttonContainer, "Köparens pris", "sell", buttonClick);
   createLink(orderForm, "köp", "kop");
+  removeElement("avanzabank_transfer_links");
 
-  const buyButton = document.getElementsByClassName("buyBtn")[0];
-  buyButton.parentNode.removeChild(buyButton);
+  removeElement("buyBtn");
 }
